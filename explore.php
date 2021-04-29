0f7c6a55623cfe226ad454100261f608b169cfaa
Louisa Evola <le9ds@virginia.edu>
2021-04-29 13:44:38 -0400

    comments updating in database

--- explore.php
+++ explore.php
@@ -7,24 +7,8 @@
 	}
 	require('db_connect.php');
 	
-	global $db;
-
-	$query = $statement = "";
-	if(!isset($_GET['genre']) || $_GET['genre'] == 'all') {
-		$query = "SELECT * FROM post";
-		$statement = $db->prepare($query);
-	}
-	else {
-		$query = "SELECT * FROM post WHERE book_genre = :genre";
-		$statement = $db->prepare($query);
-		$statement->bindValue(':genre', $_GET['genre']);
-	}
-
-	$statement->execute();
-	$post_list = $statement->fetchAll();
-	$statement->closeCursor();
-
 	function addComment($post_id, $username, $comment) {
+		global $db;
 		$query = "INSERT INTO comments VALUES (:post_id, :username, :comment)";
 
 		$statement = $db->prepare($query);
@@ -37,14 +21,31 @@
 
 	if(isset($_POST['comment_btn'])) {
 		if(!empty(trim($_POST['comment_box']))){
-			$post_id = JSON.parse(JSON.stringify(param[0]));
 			$comment_contents = htmlspecialchars(trim($_POST['comment_box']));
-			addBookToList($post_id, $_SESSION['user'], $comment_contents);
+			$post_id = htmlspecialchars(trim($_POST['hidden_post_id']));
+			echo "comment: " . $comment_contents;
+			echo " post id: " . $post_id;
+			addComment($post_id, $_SESSION['user'], $comment_contents);
 		}
 		// else {
 		// 	$_POST['err_read'] = "Error: missing field(s)";
 		// }
 	}
+
+	$filter_query = $filter_statement = "";
+	if(!isset($_GET['genre']) || $_GET['genre'] == 'all') {
+		$filter_query = "SELECT * FROM post";
+		$filter_statement = $db->prepare($filter_query);
+	}
+	else {
+		$filter_query = "SELECT * FROM post WHERE book_genre = :genre";
+		$filter_statement = $db->prepare($filter_query);
+		$filter_statement->bindValue(':genre', $_GET['genre']);
+	}
+
+	$filter_statement->execute();
+	$post_list = $filter_statement->fetchAll();
+	$filter_statement->closeCursor();
 
 ?>
 
@@ -159,14 +160,25 @@
 
 			// document.write(JSON.stringify(param));
 			
+			// COMMENT FORM
+			var comment_form = document.createElement("form");
+			// comment_form.action = "explore.php";
+			comment_form.name = "comment_form";
+			comment_form.method = "post";
+
 			var item5 = document.createElement("div");
 			item5.className = "item5";
 			var comment = document.createElement("input");
 			comment.placeholder = "type a comment...";
 			comment.name = "comment_box";
 			comment.id = "comment_box";
-			
+
 			var commentBtn = document.createElement("button");
+			var post_id = JSON.parse(JSON.stringify(param[0]));
+			var hidden_post_id = document.createElement("input");
+			hidden_post_id.type = "hidden";
+			hidden_post_id.name = "hidden_post_id";
+			hidden_post_id.value = post_id;
 			commentBtn.type = "submit";
 			commentBtn.textContent = "post"
 			commentBtn.class = "btn btn-primary";
@@ -174,19 +186,22 @@
 			commentBtn.name = "comment_btn";
 			commentBtn.id = "comment_btn";
 
-			var like = document.createElement("i");
-			like.className = "far fa-heart";
+			// var like = document.createElement("i");
+			// like.className = "far fa-heart";
 
 			// comment.appendChild(commentBtn);
 			item5.appendChild(comment);
+			item5.appendChild(hidden_post_id);
 			item5.appendChild(commentBtn);
-			item5.appendChild(like);
+			comment_form.appendChild(item5);
+			// item5.appendChild(like);
 
 			postContainer.appendChild(item1);
 			postContainer.appendChild(item2);
 			postContainer.appendChild(item3);
 			postContainer.appendChild(item4);
-			postContainer.appendChild(item5);
+			postContainer.appendChild(comment_form);
+			// postContainer.appendChild(item5);
 		}	
 </script>
 		<?php
