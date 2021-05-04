c4c434d1c74fa093dee31c497f7022d4dd891292
Matthew Kovalenko <msk9pe@virginia.edu>
2021-05-03 22:08:04 -0400

    started form additions

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 import { SESSION_STORAGE, StorageService, StorageTranscoders } from 'ngx-webstorage-service';
 import { Inject, Injectable } from '@angular/core';
 import { Book } from './book';
+//import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
@@ -20,16 +21,23 @@ export class AppComponent {
   booksRead = [];
   booksToRead = [];
 
+  user = '';
+
   ngOnInit() {
+    this.user = window.location.href.substring(28);
+
   	this.http.post<any>('http://localhost/book-review-site/profile_load.php', null).subscribe((data) => {
   		this.booksRead = data['content'][0].booksRead;
   		this.booksToRead = data['content'][0].booksToRead;
   	});
   }
 
-  book1 = new Book("harry potter", "jk rowling");
+  newBookRead = new Book('', '', 'books_read');
 
-  user = "";
+  addBookToList(form: any): void {
+    console.log(form);
+  }
+
 
   getUser(form: any): void {
   	let params = JSON.stringify(form);
--- app/book.ts
+++ app/book.ts
@@ -2,9 +2,11 @@ export class Book {
 
 	public title: string;
 	public author: string;
+	public list: string;
 
-	constructor(title: string, author: string) {
+	constructor(title: string, author: string, list: string) {
 		this.title = title;
 		this.author = author;
+		this.list = list;
 	}
 }
